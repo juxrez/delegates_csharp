4964420478ad94d5631af6b0107599b1266a98a4
Alexis Juarez <williams_juarez@hotmail.com>
2015-10-30 21:09:01 -0600

    repositorio subido

---
+++ Delegados/etHora.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Delegados
+{
+    partial class etHora
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(149, 94);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(198, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Carga del CPU";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(208, 45);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // etHora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 248);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "etHora";
+            this.Text = "Multiproceso";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Delegados/etHora.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Delegados
+{
+    public partial class etHora : Form
+    {
+        //crear el objeto del hilo secundario
+        private Thread hiloSecundario;
+        //definir como miembro de la actual forma un delegado que habilite las llamadas asincronas para acceder a la propiedad
+        //Value del progressBar
+        //Cada delegado debe ser del tipo de operacion que va a ejecutar
+        //value = int double etc         text = string,etc...
+        private delegate void setValueDelegate(int prValue);
+        private void setValue_progressBar(int hecho)
+        {   //si el progressBar necesita ser invocado ya que pudo ser llamado desde otro hilo
+            if (progressBar1.InvokeRequired)
+            {//acceso seguro a la propiedad value del progressBar
+                setValueDelegate delegado = new setValueDelegate(setValue_progressBar);
+                progressBar1.Invoke(delegado, new object[] { hecho });
+            }
+            else
+            {
+                progressBar1.Value = hecho;
+            }
+        }
+        
+        
+        //constructor
+        public etHora()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            numericUpDown1.Enabled = false;
+            progressBar1.Value = 0;
+            //tareaSecundaria();
+            //Crear el delegado que se encargará de llamar al hiloSecundario
+            ThreadStart delegado = new ThreadStart(tareaSecundaria);
+            //ThreadStart es una clase para delegados, su función es llamar al constructor de la clase Thread para iniciar un nuevo hilo
+            //Creacion del hilo
+            hiloSecundario = new Thread(delegado);
+            //ejecución dle hilo
+            hiloSecundario.Start();
+
+            //METODOS de los hilos
+            //Start = inicia un hilo
+            //sleep = detiene un hilo durante un tiempo determinado
+            //suspend = interrumpe un hilo cuando alcanza un punto determinado
+            //abort = detiene un hilo
+            //resume = revive un hilo
+            //join = deja en espera un hilo hasta que termina otro diferente. Se puede definir un tiempo de espera.
+
+            //PROPIEDADES DE UN HILO
+            //IsAlive = devuelve true si un hilo se encuentra activo
+            //IsBackGorund = indicar que un hilo será ejecutado en segundo plano si se pone como true. 
+            //name = permite obtener o establecer el nombre de un hilo
+            //priority = asigna niveles de prioridad a un hilo
+            //ThreadState = describe el estado del hilo.
+        }
+
+        private void tareaSecundaria()
+        {
+            int hecho = 0, tpHecho = 0;
+            while (hecho < numericUpDown1.Value)
+            {
+                hecho += 1;
+                tpHecho = (int)(hecho / numericUpDown1.Value * 100);
+                if (tpHecho> progressBar1.Value)
+                {
+                   // progressBar1.Value = tpHecho;
+                    //llamada al metodo donde se valora si ocupa ser invocado el delegado o no
+                    setValue_progressBar(tpHecho);
+                }
+            }
+           // button1.Enabled = true;
+           // numericUpDown1.Enabled = true;
+            //habilitar los controles que fueron deabilitados desde otro hilo
+            
+            if (hiloSecundario.ThreadState == 0)
+            {
+                Label lblHilo = new Label();
+                lblHilo.Text = "Hilo Ejecutandose";
+            }
+
+            
+            
+        }
+    }
+}
